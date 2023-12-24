7780c0b3cde735077c7535756a3768f12b354ed9
Abhijit Borkar <abhijit.smborkar@gmail.com>
2023-12-24 07:30:29 +0530

    added sidebar options and icons

--- src/Components/Player/Sidebar/Sidebar.js
+++ src/Components/Player/Sidebar/Sidebar.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./Sidebar.css";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SidebarOption from "./SidebarOption/SidebarOption";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import VideoLibraryOutlinedIcon from "@mui/icons-material/VideoLibraryOutlined";
 
 const Sidebar = () => {
   return (
-    <div>
-      <h1>This is a Sidebar</h1>
+    <div className="sidebar">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt="spotify logo"
+        className="sidebar__logo"
+      />
+      <SidebarOption title="Home" Icon={HomeOutlinedIcon} />
+      <SidebarOption title="Search" Icon={SearchOutlinedIcon} />
+      <SidebarOption title="Your Library" Icon={VideoLibraryOutlinedIcon} />
+
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
     </div>
   );
 };
